e3de3d9fa953e4993365ede9d77b3e29db1bf410
Angel Alonso <aalonsop@calpoly.edu>
2025-10-04 16:13:52 -0700

    could run on main now

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOMClient from "react-dom/client";
 import MyApp from "./MyApp";
 
-
 const container = document.getElementById("root");
-
-// Create a root
 const root = ReactDOMClient.createRoot(container);
-
-// Initial render:
 root.render(<MyApp />);
