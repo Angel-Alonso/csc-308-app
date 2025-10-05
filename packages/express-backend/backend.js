f1496eee6c90cdbf34548c34dd2cdb08c4805bd0
Angel Alonso <aalonsop@calpoly.edu>
2025-10-04 19:32:46 -0700

    Add the Id search

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,10 +4,6 @@ const app = express();
 const port = 8000;
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 const users = {
   users_list: [
     {
@@ -37,9 +33,39 @@ const users = {
     }
   ]
 };
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
 app.get("/users", (req, res) => {
-  res.send(users);
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
 });
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 
 app.listen(port, () => {
   console.log(
