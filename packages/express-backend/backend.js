df137cee990bdf179d562d480807133369381c9f
Angel Alonso <aalonsop@calpoly.edu>
2025-10-04 21:32:47 -0700

    add two features a delete and an actionto get all user that match a given name and given job

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,16 +38,28 @@ const findUserByName = (name) => {
     (user) => user["name"] === name
   );
 };
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => user.name === name && user.job === job
+  );
+};
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
-  if (name != undefined) {
+  const job = req.query.job;
+  if (name && job) {
+    let result = findUserByNameAndJob(name, job);
+    result = { users_list: result };
+    res.send(result);}
+    else if (name != undefined) {
     let result = findUserByName(name);
     result = { users_list: result };
     res.send(result);
   } else {
     res.send(users);
   }
+
+  
 });
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
@@ -61,7 +73,34 @@ app.get("/users/:id", (req, res) => {
     res.send(result);
   }
 });
+const deleteUserById = (id) => {
+  const userIndex = users["users_list"].findIndex((user) => user.id === id);
+  if (userIndex !== -1) {
+    users["users_list"].splice(userIndex, 1); 
+    return true;
+  }
+  return false;
+};
 
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const deleted = deleteUserById(id);
+  if (deleted) {
+    res.status(200).send({ message: `User with id ${id} deleted.` });
+  } else {
+    res.status(404).send({ error: "User not found." });
+  }
+});
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
